efd78c7574a2b5061395eb0672da0924b9ed0bae
Bernardo Heuer <149081961+HeuerBcH@users.noreply.github.com>
2025-08-07 14:51:38 -0300

    Update app.py

--- flask-ci-cd/app.py
+++ flask-ci-cd/app.py
@@ -1,5 +1,11 @@
 from flask import Flask
 
+# from dotenv import load_dotenv
+# import os
+# load_dotenv() 
+# DB_USER = os.getenv("DB_USER")
+# DB_PASSWORD = os.getenv("DB_PASSWORD")
+
 app = Flask(__name__)
 
 @app.route('/')
