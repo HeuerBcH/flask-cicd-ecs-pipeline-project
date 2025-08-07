bf42c5f41f7e9172ddaba4e3d17ca98dcc199990
Bernardo Heuer <bernardoheuer2005@gmail.com>
2025-08-07 18:43:30 -0300

    commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,31 @@
 from flask import Flask
+from sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+from .models import db
+from .views import views_bp
 
 def create_app():
-    app = Flask(__name__)
+    load_dotenv() # Load do .env
 
-    @app.route('/')
-    def hello():
-        return "Hello, CI/CD with Flask!"
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+    # Config do Banco
+    db_user = os.getenv('DB_USER')
+    db_password = os.getenv('DB_PASSWORD')
+    db_host = os.getenv('DB_HOST')
+    db_port = os.getenv('DB_PORT')
+    db_name = os.getenv('DB_NAME')
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = (
+        f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
+        f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+    )
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app) # Inicia o SQLAlchemy com o app
+
+    app.register_blueprint(views_bp) # Registra as rotas do views.py
 
     return app
