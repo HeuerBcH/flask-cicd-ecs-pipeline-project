b79476252a5cc47c3c13a4850a77b078950e046a
Bernardo Heuer <bernardoheuer2005@gmail.com>
2025-08-07 23:18:00 -0300

    Criando tabelas no models.py

--- app/models.py
+++ app/models.py
@@ -3,4 +3,45 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Usuario(db.Model):
-    
+    __tablename__ = 'usuarios'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(45), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
+    email = db.Columnn(db.String(120), nullable=False, unique=True)
+    empresa = db.Column(db.String(45), nullable=True)
+    criado_em = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    # Relacionament pra Tarefas onde o Usuário é Responsável
+    tarefas = db.relationship(
+        'Tarefa',
+        secondary = 'tarefa_responsaveis',
+        back_populates = 'responsaveis'
+    )
+
+    def __repr__(self):
+        return f'<Usuario {self.username}>'
+
+class Tarefa(db.Model):
+    __tablename__ = 'tarefas'
+
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(255), nullable=False)
+    descricao = db.Column(db.Text, nullable=True)
+    prioridade = db.Column(db.String(20), nullable=False)
+    status = db.Column(db.String(20), nullable=False, default='pendente')
+    prazo = db.Column(db.Date, nullable=True)
+    criado_em = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    atualizado_em = db.Column(db.DateTime, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow)
+    criado_por = db.Column(db.Integer, db.ForeignKey('usuarios.id'), nullable=False)
+
+    # Relacionamento dos Responsáveis
+    responsaveis = db.relationship(
+        'Usuario',
+        secondary = 'tarefa_responsaveis',
+        back_populates = 'tarefas'
+    )
+
+    def __repr__(self):
+        return f'<Tarefa {self.titulo}>'
+
