607cf5781570b8ad2c6884fadbebc378745d7ee7
Bernardo Heuer <bernardoheuer2005@gmail.com>
2025-08-08 14:31:48 -0300

    templates e static e routes

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,5 +1,10 @@
 from . import main_bp
+from flask import render_template
 
 @main_bp.route('/register')
 def register():
-    return "Register Page"
+    return render_template('register.html')
+
+@main_bp.route('/login')
+def login():
+    return render_template('login.html')
--- app/routes/home_routes.py
+++ app/routes/home_routes.py
@@ -1,5 +1,10 @@
 from . import main_bp
+from flask import redirect, url_for, render_template
 
 @main_bp.route('/')
-def homepahe():
-    return "Hello, CI/CD with Flask!"
+def index():
+    return redirect(url_for('main.login'))
+
+@main_bp.route('/homepage')
+def homepage():
+    return render_template("homepage.html")
